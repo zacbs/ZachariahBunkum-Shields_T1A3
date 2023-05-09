aa60415402bc1fdecf29fa31d111cbc6f8663dbe
zacbs <zacbs14@gmail.com>
2023-05-09 21:52:27 +1000

    Completed set alarm feature (3/4 tasks completed)

--- alarm.py
+++ alarm.py
@@ -1,2 +1,18 @@
-def set_alarm():
-    pass
+import datetime
+import time
+from os import system
+
+def alarm(alarm_set_point):
+    while True:
+        # Halts program to only loop once per second
+        time.sleep(1)
+        current_time = datetime.datetime.now()
+        string_time = current_time.strftime('%a, %H:%M')
+        system('cls')
+        system('clear')
+        print('Current time: '+ string_time)
+        alarm_set_time = str(current_time.strftime('%a') + ', ' + alarm_set_point)
+        print('Alarm time: '+ alarm_set_time)
+        if string_time == alarm_set_time:
+            print('Wake up!')
+            break
--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import datetime
+from alarm import alarm
 
-from alarm import set_alarm
-# Prints current time
-current_time = datetime.datetime.now(tz=None).strftime('%H:%M')
-
-print(current_time)
-
-alarm_time = input(
-    'What time do you want to set the alarm for (Format: 24hr time, 00:00)?')
-
-set_alarm()
+alarm(input('What time would you like to set the alarm for?'))
