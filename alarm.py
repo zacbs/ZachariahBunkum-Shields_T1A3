2b90a6e5f32c20c700e5d2908dccedc1e3362427
zacbs <zacbs14@gmail.com>
2023-05-11 02:54:49 +1000

    Completed individual alarm page function

--- alarm.py
+++ alarm.py
@@ -5,24 +5,28 @@ from os import system
 
 # Exception handling for file creation and setting up the file for use if it is not correctly setup
 
-sample_header = ['alarm_time', 'alarm_on', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+sample_header = ['alarm_time', 'alarm_on', 'Monday', 'Tuesday',
+                 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
 
 def file_create():
     try:
         with open('saved_alarms.csv', 'x', newline='') as f:
-            writer = csv.writer(f)            
+            writer = csv.writer(f)
             writer.writerow(sample_header)
-            pass       
+            pass
     except FileExistsError:
         with open('saved_alarms.csv', 'r', newline='') as f:
             reader = csv.DictReader(f)
             header = reader.fieldnames
         if header != sample_header:
             with open('saved_alarms.csv', 'w', newline='') as f:
-                writer = csv.writer(f)            
+                writer = csv.writer(f)
                 writer.writerow(sample_header)
 
 # TODO: Refactor core alarm logic out of this function into main.py
+
+
 def alarm(alarm_set_point):
     while True:
         # Halts program to only loop once per second
@@ -32,7 +36,8 @@ def alarm(alarm_set_point):
         system('cls')
         system('clear')
         print('Current time: ' + string_time)
-        alarm_set_time = str(current_time.strftime('%a') + ', ' + alarm_set_point)
+        alarm_set_time = str(current_time.strftime(
+            '%a') + ', ' + alarm_set_point)
         print('Alarm time: ' + alarm_set_time)
         if string_time == alarm_set_time:
             print('Wake up!')
@@ -40,10 +45,13 @@ def alarm(alarm_set_point):
             break
 
 # Saves current set alarm
+
+
 def save_alarm(alarm_set_point):
     with open('saved_alarms.csv', 'a', newline='') as f:
-        writer = csv.DictWriter(f,fieldnames=sample_header)
-        writer.writerow({'alarm_time':alarm_set_point, 'alarm_on':True, 'Monday':False, 'Tuesday':False, 'Wednesday':False, 'Thursday':False, 'Friday':False, 'Saturday':False, 'Sunday':False,})
+        writer = csv.DictWriter(f, fieldnames=sample_header)
+        writer.writerow({'alarm_time': alarm_set_point, 'alarm_on': True, 'Monday': False, 'Tuesday': False,
+                        'Wednesday': False, 'Thursday': False, 'Friday': False, 'Saturday': False, 'Sunday': False, })
 
 
 def value_toggle(target_row, target_column, target_value):
--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def menu_saved():
     print('Current saved alarms:')
     with open('saved_alarms.csv', 'r') as f:
@@ -12,8 +13,17 @@ def menu_saved():
                 alarm_state = 'ON'
             else:
                 alarm_state = 'OFF'
-            print(f'{number + 1}: {row["alarm_time"]}, alarm is currently {alarm_state}')
-    print('Press corresponding key to edit that alarm')
-    print('Press q to go back to the main menu')
+            print(
+                f'{number + 1}: {row["alarm_time"]}, alarm is currently {alarm_state}')
+    print('Press corresponding key to edit that alarm.\nPress q to go back to the main menu.')
+    return input()
 
 
+def menu_saved_alarm(alarm_time):
+    print(
+        f'Currently editing {alarm_time} alarm.\nPress 1 to turn this alarm on.\nPress 2 to turn on reoccuring alarm.\nPress 3 to delete the current alarm.')
+    return input()
+
+
+menu_saved_alarm('11:30')
+# print(menu_saved())
